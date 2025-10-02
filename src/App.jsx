310abceb375c791123d8b68e9c84fe70910e7d35
Abela16 <abelyohanis016@gmail.com>
2025-10-02 08:17:45 -0700

    feat: add weather fetching logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weather, setweather] = useState(null);
+
+  const getweather = async (city) => {
+    const apikey = "e359c652283bbb8ea2dcddd7227a4d50"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setweather(data);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='app'>
+      <h1 className='title'>Weather App</h1>
+      <SearchBox onSearch={getweather}/>
+      {weather && <weatherCard data={weather} />}
+
+    </div>
     </>
-  )
+  );
 }
 
 export default App
