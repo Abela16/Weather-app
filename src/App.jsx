e10c13c749f3bd91bf8a9389ce5f5e637173cf4b
Abela16 <abelyohanis016@gmail.com>
2025-10-02 09:06:30 -0700

    fixing the problem with api key

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [weather, setweather] = useState(null);
 
   const getweather = async (city) => {
-    const apikey = "e359c652283bbb8ea2dcddd7227a4d50"
+    const apikey =import.meta.env.VITE_WEATHER_API_KEY;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
